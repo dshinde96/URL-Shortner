563defd5acec0cd3505d89a38e434031756b1a90
dshinde <dnyaneshwarshinde33368@gmail.com>
2024-01-04 09:45:36 +0530

    Introduced hashing of password using bcryptjs

--- Controller/User.js
+++ Controller/User.js
@@ -1,22 +1,39 @@
-const User=require('../Models/user');
-const{setUser}=require('../Services/auth');
+const User = require('../Models/user');
+const { setUser } = require('../Services/auth');
+const bcrypt = require('bcrypt');
 
 
-const handleUserSignup=async(req,res)=>{
-    const {name,email,password}=req.body;
-    const user=await User.create({name,email,password});
-    const authTocken=setUser(user);
-    res.cookie('authTocken',authTocken);
-    return res.redirect("/");   //localhost/ with get method
+const handleUserSignup = async (req, res) => {
+    const { name, email, password } = req.body;
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+            if (err) {
+                return res.redirect('/signup');
+            }
+            const user = await User.create({ name, email, password:hash,role:"Normal" });
+            const authTocken = setUser(user);
+            res.cookie('authTocken', authTocken);
+            return res.redirect("/");   //localhost/ with get method
+        });
+    });
+
 }
-const handleUserLogin=async(req,res)=>{
-    const {email,password}=req.body;
-    const user=await User.findOne({email,password});
-    if(!user){
-        return res.render('login',{msg:"invalid credentials"});
+const handleUserLogin = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.render('login', { msg: "invalid credentials" });
     }
-    const authTocken=setUser(user);
-    res.cookie('authTocken',authTocken);
-    return res.redirect("/");
+    bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
+            const authTocken = setUser(user);
+            res.cookie('authTocken', authTocken);
+            return res.redirect("/");
+        }
+        else{
+            return res.redirect('/signup');
+        }
+    });
+
 }
-module.exports={handleUserSignup,handleUserLogin}
+module.exports = { handleUserSignup, handleUserLogin }
--- Routes/ShortURL.js
+++ Routes/ShortURL.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const {handleURLShortner}=require('../Controller/ShortURL');
+
+
 router.route('/:shortid').get(handleURLShortner);
 
 module.exports=router;
